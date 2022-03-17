afc7726e1e9da382b1d80b1bc97a3e213aa7e5aa
Joe Froula <jmfroula@gmail.com>
2022-03-16 23:38:17 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+
+  const [state, updateState] = useState(0)
+  
+  const increment = () => {
+    updateState(state + 1)
+  }
+
+  const decrement = () => {    
+    updateState(state - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <h1>{state}</h1>
+      <div>
+        <Button action={decrement} text="Decrement"/> 
+        <Button action={increment} text="Increment"></Button> 
+      </div> 
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+function Button({action, text}) {
+  return (
+    <button onClick={action}>
+        {text}
+    </button>
+  ) 
+}
+
+export default Button
